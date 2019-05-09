67165a8a47768fd4845b287d666271a88b467639
Carissa Allen <carissawood@outlook.com>
2019-05-08 17:59:43 -0700

    Develop application entry point (start script)

---
+++ app.js
@@ -0,0 +1,22 @@
+// app.js - application route module
+
+const express = require('express');
+const port = process.env.PORT;
+const app = express();
+const router = express.Router();
+const path = __dirname + '/views/';
+
+app.use(express.static(__dirname)); // serves static files
+
+router.get('/', function(req, res) {
+    res.sendFile(path + 'index.html');
+});
+
+app.use('/', router);
+app.listen(port);
+
+// future work: 
+// should add error page to handle erroneous requests--
+// router.get('*', function(req, res) {
+//     res.sendFile('error.html');
+// });
