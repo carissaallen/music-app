9f4cdab68ecb4f04a78a0cf1642029cc46017bc7
Carissa Allen <carissawood@outlook.com>
2019-05-23 23:42:33 -0700

    Add script to toggle input placeholder text

---
+++ scripts/toggle.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#toggle-event").change(function () {
+        if ($(this).prop("checked")) {
+            $('input:text').attr("placeholder", "Type an artist name");
+        } else {
+            $('input:text').attr("placeholder", "Type a song name");
+        }
+    })
+});
