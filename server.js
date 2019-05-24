0c2d4b5ed634ae268095b331512f448b43638f62
Carissa Allen <carissawood@outlook.com>
2019-05-24 16:27:22 -0700

    Add endpoint to return playlist from track seed

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.static(__dirname)); // Serves static files
 app.use(express.static(__dirname + '/views/')) 
    .use(cookieParser());
 
+
 // Render main page
 router.get('/', function(req, res) {
   res.sendFile(path + 'index.html');
@@ -27,16 +28,19 @@ res.sendFile(path + 'about.html');
 
 app.use('/', router);
 
+
 // Authenticate against the Spotify accounts
 var client_id = process.env.client_id;          // Application client
 var client_secret = process.env.client_secret;  // Application secret
 var redirect_uri = process.env.redirect_uri;    // Application redirect uri
+
 
 // Create the api object with the credentials
 var spotifyApi = new SpotifyWebApi({
   clientId: client_id,
   clientSecret: client_secret
 });
+
 
 // Retrieve an access token from Spotify API
 spotifyApi.clientCredentialsGrant().then(
@@ -53,8 +57,9 @@ spotifyApi.clientCredentialsGrant().then(
   }
 );
 
-// Retrieve recommended playlist
-router.get('/playlist', function(req, res) {
+
+// Retrieve recommended playlist based on artist
+router.get('/artist/playlist', function(req, res) {
   query = req.query;
   var artist = query['input'];
   
@@ -100,6 +105,56 @@ router.get('/playlist', function(req, res) {
       console.error(err);
     });
 });
+
+
+// Retrieve recommended playlist based on song
+router.get('/song/playlist', function(req, res) {
+  query = req.query;
+  var song = query['input'];
+  
+  spotifyApi
+    .searchTracks(song)
+    .then(function(data) {
+      var track_id = data.body.tracks.items[0].id;
+      return track_id
+    })
+    .then(function(track_id) {
+      return spotifyApi.getRecommendations({ seed_tracks: [track_id] });
+    })
+    .then(function(data){
+      var playlist = [];
+      var previewURL = '';
+      var tracks = data.body.tracks;
+
+      for (var i=0; i < tracks.length; i++) {
+        var track = {
+          id: tracks[i].id,
+          song: tracks[i].name,
+          artist: tracks[i].artists[0].name,
+          album: tracks[i].album.name,
+          albumImageURL: tracks[i].album.images[0].url,
+          previewURL: tracks[i].preview_url
+        }
+        playlist.push(track);
+      }
+      return {
+        "playlist": playlist
+      }
+    })
+    .then(function(playlistObj) {
+      fs.readFile(path + 'playlist.html', function(err, data) {
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+        res.write(mustache.render(data.toString(), playlistObj));
+        res.end();
+      });
+    }) 
+    .catch(function(err) {
+      console.error(err);
+    });
+});
+
 
 console.log(`Listening on port...`);
 app.listen(port);
