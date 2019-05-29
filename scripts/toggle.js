7c46d3627bee51d864b3de0b215ef78862879eca
Carissa Allen <carissawood@outlook.com>
2019-05-29 13:24:36 -0700

    Final attempt to get mobile toggle to work

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,10 +1,21 @@
 $(function() {
-    $("#toggle-event").on('touch change', function() {
-        if ($(this).prop("checked")) {
-            $('input:text').attr("placeholder", "Type an artist name");
-        } else {
-            $('input:text').attr("placeholder", "Type a song name");
-            $('form').attr('action', '/song/playlist');
-        }
-    })
+  $("#toggle-event").on("change", function() {
+    if ($(this).prop("checked")) {
+      $("input:text").attr("placeholder", "Type an artist name");
+    } else {
+      $("input:text").attr("placeholder", "Type a song name");
+      $("form").attr("action", "/song/playlist");
+    }
+  });
+});
+
+$(function() {
+  $(".toggle").on("touchstart", function() {
+    if ($(this).prop("checked")) {
+      $("input:text").attr("placeholder", "Type an artist name");
+    } else {
+      $("input:text").attr("placeholder", "Type a song name");
+      $("form").attr("action", "/song/playlist");
+    }
+  });
 });
