0a13bfe17ebceb5033078d43ffa30d2e0fe8118b
Carissa Allen <carissawood@outlook.com>
2019-05-29 17:47:55 -0700

    (Test) Testing toggle on mobile

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -10,11 +10,15 @@ $(function() {
 });
 
 $(function() {
-  $("#toggle-event").on("tap", function() {
+  $(".toggle").on("touch", function() {
     if ($(this).prop("checked")) {
       $("input:text").attr("placeholder", "Type an artist name");
+      $("input:checkbox").attr("data-on", "Artist");
+      $("input:checkbox").attr("data-onstyle", "dark");
     } else {
       $("input:text").attr("placeholder", "Type a song name");
+      $("input:checkbox").attr("data-off", "Song");
+      $("input:checkbox").attr("data-offstyle", "default");
       $("form").attr("action", "/song/playlist");
     }
   });
