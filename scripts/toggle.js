1553c22f17a8b6c39e6b465192f437bf93d3d4f7
Carissa Allen <carissawood@outlook.com>
2019-05-29 17:29:12 -0700

    Use tap event for toggle mobile

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -8,3 +8,14 @@ $(function() {
     }
   });
 });
+
+$(function() {
+  $("#toggle-event").on("tap", function() {
+    if ($(this).prop("checked")) {
+      $("input:text").attr("placeholder", "Type an artist name");
+    } else {
+      $("input:text").attr("placeholder", "Type a song name");
+      $("form").attr("action", "/song/playlist");
+    }
+  });
+});
