5c5f7449c14593157297b23936b7fe9605a61021
Carissa Allen <carissawood@outlook.com>
2019-06-05 15:32:34 -0700

    Add action for toggle on artist

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -2,6 +2,7 @@ $(function() {
   $("#toggle-event").on("touchstart change", function(event) {
     if ($(this).prop("checked")) {
       $("input:text").attr("placeholder", "Type an artist name");
+      $("form").attr("action", "/artist/playlist");
     } else {
       $("input:text").attr("placeholder", "Type a song name");
       $("form").attr("action", "/song/playlist");
